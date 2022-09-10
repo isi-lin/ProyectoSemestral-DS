c8a0fc205b97958a55aa9ee7fd8b290041a216e8
Isidora Lin <ilin@alumnos.uai.cl>
2022-09-10 13:29:18 -0400

    actualizar

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import BasicExample from './components/Navbar1';
+import Carousel1 from './components/Carousel1';
+
+function App() {
+  return (
+    <div className="App">
+      <BasicExample></BasicExample>
+      <Carousel1></Carousel1>
+
+      
+      
+    </div>
+  );
+}
+
+export default App;
