fd1890d1cca8163a184f2b7d38edb464f570a586
Isidora Lin <ilin@alumnos.uai.cl>
2022-09-10 15:14:20 -0400

    actualizacion navbar

--- src/components/Navbar1.jsx
+++ src/components/Navbar1.jsx
@@ -2,26 +2,41 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import imagen_logo from '../logo.png'
 
 function BasicExample() {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+      <img
+              alt=""
+              src={imagen_logo}
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />{' '}
+        <Navbar.Brand href="#home">Heikala</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+            <Nav.Link href="https://www.instagram.com/heikala/" target="_blank" rel="noopener noreferrer">Instagram</Nav.Link>
+            <Nav.Link href="#link">Colaborations</Nav.Link>
+            <NavDropdown title="Shop" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Prints</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
-                Another action
+                Art Supplies
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Books</NavDropdown.Item>
               <NavDropdown.Divider />
               <NavDropdown.Item href="#action/3.4">
                 Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+            <NavDropdown title="Info" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">About Heikala</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Shipping
               </NavDropdown.Item>
             </NavDropdown>
           </Nav>
