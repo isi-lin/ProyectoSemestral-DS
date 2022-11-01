64fb6f49879b404fc41376d6da1d2cd0da40c713
Yin Zhong Cai <yzhong@alumnos.uai.cl>
2022-11-01 00:57:30 -0300

    actualizacion footer

--- src/components/Footer1.jsx
+++ src/components/Footer1.jsx
@@ -3,34 +3,8 @@ import { MDBFooter, MDBContainer, MDBRow, MDBCol, MDBIcon } from 'mdb-react-ui-k
 
 export default function Footer1() {
   return (
-    <MDBFooter bgColor='none' className='text-center text-lg-start text-muted'>
-      <section className='d-flex justify-content-center justify-content-lg-between p-4 border-bottom'>
-        <div className='me-5 d-none d-lg-block'>
-          <span>Get connected with us on social networks:</span>
-        </div>
-
-        <div>
-          <a href='' className='me-4 text-reset'>
-            <MDBIcon fab icon="facebook-f" />
-          </a>
-          <a href='' className='me-4 text-reset'>
-            <MDBIcon fab icon="twitter" />
-          </a>
-          <a href='' className='me-4 text-reset'>
-            <MDBIcon fab icon="google" />
-          </a>
-          <a href='' className='me-4 text-reset'>
-            <MDBIcon fab icon="instagram" />
-          </a>
-          <a href='' className='me-4 text-reset'>
-            <MDBIcon fab icon="linkedin" />
-          </a>
-          <a href='' className='me-4 text-reset'>
-            <MDBIcon fab icon="github" />
-          </a>
-        </div>
-      </section>
-
+    <MDBFooter bgColor='light' className='text-center text-lg-start text-muted'>
+      <section className='d-flex justify-content-center justify-content-lg-between p-4 border-bottom'></section>
       <section className=''>
         <MDBContainer className='text-center text-md-start mt-5'>
           <MDBRow className='mt-3'>
@@ -39,76 +13,83 @@ export default function Footer1() {
                 <MDBIcon icon="gem" className="me-3" />
                 Company name
               </h6>
-              <p>
+              <div class='footercolor'>
+              
+              
                 Here you can use rows and columns to organize your footer content. Lorem ipsum dolor sit amet,
                 consectetur adipisicing elit.
-              </p>
+              
+              </div>
             </MDBCol>
 
             <MDBCol md="2" lg="2" xl="2" className='mx-auto mb-4'>
+              <div className='footercolor'>
+
               <h6 className='text-uppercase fw-bold mb-4'>Products</h6>
-              <p>
+              
                 <a href='#!' className='text-reset'>
                   Angular
                 </a>
-              </p>
-              <p>
+              
+              <div className='footercolor'>
                 <a href='#!' className='text-reset'>
                   React
                 </a>
-              </p>
-              <p>
+              </div>
+
+              <div className='footercolor'>
                 <a href='#!' className='text-reset'>
                   Vue
                 </a>
-              </p>
-              <p>
+              </div>
+              <div className='footercolor'>
                 <a href='#!' className='text-reset'>
                   Laravel
                 </a>
-              </p>
+              </div>
+              </div>
             </MDBCol>
 
             <MDBCol md="3" lg="2" xl="2" className='mx-auto mb-4'>
               <h6 className='text-uppercase fw-bold mb-4'>Useful links</h6>
-              <p>
+              <div className='footercolor'>
                 <a href='#!' className='text-reset'>
                   Pricing
                 </a>
-              </p>
-              <p>
+              </div>
+              <div className='footercolor'>
                 <a href='#!' className='text-reset'>
                   Settings
                 </a>
-              </p>
-              <p>
+              </div>
+              <div className='footercolor'>
                 <a href='#!' className='text-reset'>
                   Orders
                 </a>
-              </p>
-              <p>
+              </div>
+              <div className='footercolor'>
                 <a href='#!' className='text-reset'>
                   Help
                 </a>
-              </p>
+              </div>
             </MDBCol>
 
             <MDBCol md="4" lg="3" xl="3" className='mx-auto mb-md-0 mb-4'>
               <h6 className='text-uppercase fw-bold mb-4'>Contact</h6>
-              <p>
+              <div className='footercolor'>
                 <MDBIcon icon="home" className="me-2" />
                 New York, NY 10012, US
-              </p>
-              <p>
+              </div>
+              <div className='footercolor'>
                 <MDBIcon icon="envelope" className="me-3" />
                 info@example.com
-              </p>
-              <p>
+              </div>
+              <div className='footercolor'>
                 <MDBIcon icon="phone" className="me-3" /> + 01 234 567 88
-              </p>
-              <p>
+              </div>
+              <div>
                 <MDBIcon icon="print" className="me-3" /> + 01 234 567 89
-              </p>
+              </div>
             </MDBCol>
           </MDBRow>
         </MDBContainer>
