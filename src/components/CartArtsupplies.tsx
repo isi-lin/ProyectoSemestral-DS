e3e3d87a0c858bb3f948565c2b68f4b51d03465a
Isidora Lin <ilin@alumnos.uai.cl>
2022-11-05 22:47:59 -0300

    shopping cart funciona

---
+++ src/components/CartArtsupplies.tsx
@@ -0,0 +1,36 @@
+import { useShoppingCart } from "./ShoppingCartContext.tsx"
+import artsuppliesItems from "../itemsartsupplies.json"
+import { Stack } from "react-bootstrap"
+import React from 'react'
+import { Button } from "react-bootstrap"
+
+
+type CartItemProps = {
+    id: number
+    quantity: number
+
+}
+
+export function CartItem2 ({id, quantity}: CartItemProps) {
+    const { removeFromCart} = useShoppingCart()
+    const item = artsuppliesItems.find( i => i.data.id === id)
+    if (item == null) return null
+
+    return (
+        <Stack direction = "horizontal" gap ={2} className= "d-flex align-items-center">
+            <img src={item.data.img} style ={{width: "125px", height: "75px", objectFit: "cover"}}/>
+            <div className= "me-auto">
+                <div>
+                    {item.data.name} {quantity > 1 && <span className= "text-muted" style={{fontSize: ".65rem"}}>x{quantity}</span>}
+                </div>
+                <div className ="text-muted" style={{fontSize :".75rem"}}>{item.data.price}
+                </div>
+            </div>
+            <div>{item.data.price * quantity}</div>
+            <Button variant ="outline-danger" size="sm" onClick={()=> removeFromCart(item.data.id)}>&times;</Button>
+
+        </Stack>
+    )
+
+
+}
--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -4,6 +4,10 @@ import React from 'react'
 import { useShoppingCart } from "./ShoppingCartContext.tsx";
 import { CartItem } from "./CartPrints.tsx";
 import printsItems from "../itemsprints.json"
+import { CartItem2 } from "./CartArtsupplies.tsx";
+import { CartItem3 } from "./CartBooks.tsx";
+import artsuppliesItems from "../itemsartsupplies.json"
+import booksItems from "../itemsbooks.json"
 
 type ShoppingCartProps = {
     isOpen: boolean
@@ -16,13 +20,28 @@ export function ShoppingCart({isOpen}: ShoppingCartProps){
             <Offcanvas.Title>Cart</Offcanvas.Title>
         </Offcanvas.Header>
         <OffcanvasBody>
+             <Stack gap = {3}>
+                {cartItems.map(item => (
+                <CartItem2 key = {item.id} {...item} />))}
+                
+
+            </Stack>
+            <Stack gap = {3}>
+                {cartItems.map(item => (
+                <CartItem3 key = {item.id} {...item} />))}
+                
+
+            </Stack>
             <Stack gap = {3}>
                 {cartItems.map(item => (
                 <CartItem key = {item.id} {...item} />))}
+
                 <div className="ms-auto fw-bold fs-5">
                     Total {cartItems.reduce((total, cartItem) => {
                         const item = printsItems.find(i => i.data.id === cartItem.id)
-                        return total + (item?.data.price || 0) * cartItem.quantity
+                        const item2 = artsuppliesItems.find(i => i.data.id === cartItem.id)
+                        const item3 = booksItems.find(i => i.data.id === cartItem.id)
+                        return total + (item?.data.price || 0) * cartItem.quantity + (item2?.data.price || 0) * cartItem.quantity + (item3?.data.price || 0) * cartItem.quantity
                     }, 0)
                     }
                 </div>
